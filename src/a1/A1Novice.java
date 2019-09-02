afd53e9663ab7c83d920931c96a82a2ff6d58278
kassiayg <kassiayg@kassiayg-pc>
2019-09-02 01:51:35 -0400

    Completed novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,36 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		
+		
+		int count = scan.nextInt();
+		
+		for (int j=0; j <count; j++) {
+
+		String nameOfCustomer = scan.next();
+		String letter = Character.toString(nameOfCustomer.charAt(0));
+		nameOfCustomer = letter;
+		nameOfCustomer = nameOfCustomer + ". "+ scan.next();
+		
+		int items = scan.nextInt();
+		double total = 0;
+		for (int i=0; i<items; i++) {
+			double y = 0;
+			int x = scan.nextInt();
+			String itemName = scan.next();
+			y = scan.nextDouble();
+			double z = y;
+			for (int k=1; k <x; k++) {
+				z = z + y;
+			}
+			total = total + z;
+		}		
+				
+		System.out.println(nameOfCustomer + ": " + total);
+
+	}
+		
+		scan.close();
+		
 	}
 }
